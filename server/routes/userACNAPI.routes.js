fcd5e99ec49989768be6059b678a0e0e363069f9
teezhiyao <teezhiyao@gmail.com>
2019-03-10 21:38:16 +0800

    User Api (Untested)
    Also added keys to gitignore

--- server/routes/userACNAPI.routes.js
+++ server/routes/userACNAPI.routes.js
@@ -1,28 +1,98 @@
 import { Router } from "express";
-
+import apitoken from "../../nopush";
 const router = new Router();
-
+const token = apitoken;
+const userUrl =
+  "https://ug-api.acnapiv3.io/swivel/acnapi-common-services/common/users";
 var request = require("request");
 
-var options = {
-  method: "POST",
-  url: "https://ug-api.acnapiv3.io/swivel/acnapi-common-services/common/users",
-  headers: {
-    "cache-control": "no-cache",
-    "Server-Token":
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlF6Y3hRVEl5UkRVeU1qYzNSakEzTnpKQ01qVTROVVJFUlVZelF6VTRPRUV6T0RreE1UVTVPQSJ9.eyJpc3MiOiJodHRwczovL2FjbmFwaS1wcm9kLmF1dGgwLmNvbS8iLCJzdWIiOiI1cDZnbzhaNWtwTlI5bHk4U1lONEh5RlVTZEp2WE5oZkBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9wbGFjZWhvbGRlci5jb20vcGxhY2UiLCJpYXQiOjE1NDk5NTI5MDEsImV4cCI6MTU1MjU0NDkwMSwiYXpwIjoiNXA2Z284WjVrcE5SOWx5OFNZTjRIeUZVU2RKdlhOaGYiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.cPm5fS0jQ2R60dtG5gsL97g0ukUGogB9KjbinzvOPHMCmC1YB58tnYTJle3BnG3DePzpQTQlpB9QSaOyE8uXsmNb1rFfOK9oFLLJ9pU7912oYp8KAHfXxtyYs_ajZ6Q9SWbYvPD9OAm5ZhpIb4OmQ4pUkwxoUwWejsHzh0K7u1987X-_wJt-XIb0vn1twJFoTI0qZ_pXwOo7TKNsjYDJkvvA3em-S2CYDqJmD7Nqsg3xEf0yPoH9SQiojqHQt7hgcsMymFVYuV5SOkYknMn3TFHGFQI-iQ3zftKm3iv6i3oxcFEceuOdjPGjXUSWN08xS6gJbxc5N9yzIBB6rgRI8A",
-    "Content-Type": "application/json"
-  },
-  body: { username: "lmao", password: "nihaoma", phone: "415-392-0202" },
-  json: true
-};
-//Creating a user
-router.route("/createUser").post(function(req, res) {
-  console.log(req);
+router.route("/userRegister").post(function(req, res, next) {
+  var options = {
+    method: "POST",
+    url: userUrl,
+    headers: {
+      "cache-control": "no-cache",
+      "Server-Token": token,
+      "Content-Type": "application/json"
+    },
+    body: req.body,
+    json: true
+  };
   request(options, function(error, response, body) {
-    console.log("error:", error); // Print the error if one occurred and handle it
-    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-    res.send(body);
+    if (error) throw new Error(error);
+    console.log(body);
+  });
+});
+
+router.route("/queryAll").get(function(req, res, next) {
+  var options = {
+    method: "GET",
+    url: `${userUrl}/users`,
+    headers: {
+      "cache-control": "no-cache",
+      "Server-Token": token,
+      "Content-Type": "application/json"
+    }
+  };
+  request(options, function(error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+  });
+});
+
+router.route("/updateUser").put(function(req, res, next) {
+  const username = user;
+  const sessionToken = 1029382;
+  var options = {
+    method: "PUT",
+    url: `${userUrl}/${username}`,
+    headers: {
+      "cache-control": "no-cache",
+      "X-Parse-Session-Token": sessionToken,
+      "Server-Token": token,
+      "Content-Type": "application/json"
+    },
+    body: req.body
+  };
+  request(options, function(error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+  });
+});
+
+router.route("/getCurrentUser").get(function(req, res, next) {
+  var options = {
+    method: "GET",
+    url: `${userUrl}/me`,
+    headers: {
+      "cache-control": "no-cache",
+      "X-Parse-Session-Token": sessionToken,
+      "Server-Token": token,
+      "Content-Type": "application/json"
+    }
+  };
+  request(options, function(error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+  });
+});
+
+router.route("/deleteUser").delete(function(req, res, next) {
+  const username = user;
+  const sessionToken = 1029382;
+  var options = {
+    method: "DELETE",
+    url: `${userUrl}/${username}`,
+    headers: {
+      "cache-control": "no-cache",
+      "X-Parse-Session-Token": sessionToken,
+      "Server-Token": token,
+      "Content-Type": "application/json"
+    }
+  };
+  request(options, function(error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
   });
 });
 
