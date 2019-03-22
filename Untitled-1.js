8bc5d634a29dc1d43e5c20846b68d88d87dfd9ea
nashitaabd <nashitag@gmail.com>
2019-03-22 15:53:03 +0800

    package json with server crawler
    package json with server crawler

---
+++ Untitled-1.js
@@ -0,0 +1,59 @@
+var Crawler = require("js-crawler");
+ 
+new Crawler().configure({depth: 3})
+  .crawl("http://localhost:8100", function onSuccess(page) {
+    console.log(page.url);
+  });
+  
+
+// Options-based API
+// Alternative APIs for passing callbacks to the crawl function.
+// var Crawler = require("js-crawler");
+ 
+// var crawler = new Crawler().configure({ignoreRelative: false, depth: 2});
+ 
+// crawler.crawl({
+//   url: "http://localhost:8100",
+//   success: function(page) {
+//     console.log(page.url);
+//   },
+//   failure: function(page) {
+//     console.log(page.status);
+//   },
+//   finished: function(crawledUrls) {
+//     console.log(crawledUrls);
+//   }
+// });
+
+
+
+
+// //Handling errors
+// //It is possible to pass an extra callback to handle errors, consider the modified example above:
+// var Crawler = require("js-crawler");
+ 
+// new Crawler().configure({depth: 3})
+//   .crawl("http://localhost:8100", function(page) {
+//     console.log(page.url);
+//   }, function(response) {
+//     console.log("ERROR occurred:");
+//     console.log(response.status);
+//     console.log(response.url);
+//     console.log(response.referer);
+//   });
+
+
+
+
+// //Knowing when all crawling is finished
+// //Extra callback can be passed that will be called when all the urls have been crawled and crawling has finished. All crawled urls will be passed to that callback as an argument.
+
+// var Crawler = require("js-crawler");
+ 
+// new Crawler().configure({depth: 2})
+//   .crawl("http://localhost:8100", function onSuccess(page) {
+//     console.log(page.url);
+//   }, null, function onAllFinished(crawledUrls) {
+//     console.log('All crawling finished');
+//     console.log(crawledUrls);
+//   });
