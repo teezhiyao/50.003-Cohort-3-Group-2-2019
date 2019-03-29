43fab74cc58fbc02c3c5b41e2f575d10948d779a
teezhiyao <teezhiyao@gmail.com>
2019-03-30 03:39:28 +0800

    Login Page
    Login Page from Nash
    
    Co-Authored-By: nashitag <nashitag@users.noreply.github.com>

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@
 import React, { Component } from "react";
 import { Route, IndexRoute } from "react-router";
 import App from "./modules/App/App";
-import { Loggedin } from "./modules/Login/pages/Loggedin";
+import { Login } from "./modules/Login/Login";
 
 // require.ensure polyfill for node
 if (typeof require.ensure !== "function") {
@@ -53,10 +53,7 @@ export default (
       path="/Loggedin"
       getComponent={(nextState, cb) => {
         require.ensure([], require => {
-          cb(
-            null,
-            require("../client/src/containers/Base/HeaderContainer").default
-          );
+          cb(null, require("./modules/Login/Login").default);
         });
       }}
     />
