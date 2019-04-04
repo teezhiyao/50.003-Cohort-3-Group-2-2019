f558312972863cbb03a25805c3aefbdbf7f84579
teezhiyao <teezhiyao@gmail.com>
2019-04-04 09:55:09 +0800

    Temp Login
    Error reloading home after calling function handleSubmit. Suspect issue to be fetchLogin

--- client/modules/LoginAllUpdated/Login.js
+++ client/modules/LoginAllUpdated/Login.js
@@ -5,20 +5,30 @@ import Bootstrap from "react-bootstrap";
 import "./Login.css";
 import { fetchLogin } from "../Post/PostActions";
 import { Link } from "react-router";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import { browserHistory } from "react-router";
 
-export default class Login extends Component {
+class Login extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       email: "",
-      password: ""
+      password: "",
+      redirect: false
     };
   }
 
   validateForm() {
     return this.state.email.length > 0 && this.state.password.length > 0;
   }
+
+  // returnRedirect() {
+  //   return this.state.redirectToReferrer;
+  // }
 
   handleChange = event => {
     console.log(event.target.value);
@@ -27,15 +37,50 @@ export default class Login extends Component {
     });
   };
 
+  // renderRedirect = () => {
+  //   if (this.state.redirect) {
+  //     this.setState({ redirect: false });
+  //     return <Redirect to={{ pathname: `/` }} />;
+  //   }
+  // };
+
+  // nextPage() {
+  //   this.setState((state, prop) => {
+  //     return {
+  //       page: state.page + 1
+  //     };
+  //   });
+  // }
+
   handleSubmit = event => {
     event.preventDefault();
-
+    // console.log(this.state.redirect);
     try {
       const email = this.state.email;
       const password = this.state.password;
-      // console.log(props.user);
+      // this.props.dispatch(fetchLogin(email, password));
+      console.log("IN HANDLE SUBMIT");
+      // this.props
+      //   .dispatch(fetchLogin(email, password))
+      //   .then(function(response) {
+      //     console.log("Success!", response.user.objectId);
+      //     return response.user.objectId;
+      //   });
+
       if (confirm("Do you want to Log in")) {
-        fetchLogin(email, password);
+        this.props.dispatch(fetchLogin(email, password)).then(
+          function(response) {
+            if (response.user.objectId === "XHNhUvlgx5") {
+              console.log("Success!", response.user.objectId);
+              browserHistory.push("/SignUpPage");
+              // this.setState(() => {
+              //   return {
+              //     redirect: true
+              //   };
+              // });
+            }
+          }.bind(this)
+        );
       }
     } catch (e) {
       alert(e.message);
@@ -44,37 +89,68 @@ export default class Login extends Component {
 
   render() {
     console.log("handling submit");
+
+    // if (this.state.redirectToReferrer) {
+    //   return <Redirect to="/" />;
+    // }
     return (
       <div className="Login">
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Group controlId="email" bssize="large">
-            <Form.Control
-              autoFocus
-              type="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </Form.Group>
-          <Form.Group controlId="password" bssize="large">
-            <Form.Control
-              value={this.state.password}
-              onChange={this.handleChange}
-              type="password"
-            />
-          </Form.Group>
-          <Button
-            // block
-            bssize="large"
-            disabled={!this.validateForm()}
-            type="submit"
-          >
-            Login
-          </Button>
-        </Form>
-        <Link to={`/SignUpPage`}>
-          <Button type="signup">Sign Up</Button>
-        </Link>
+        {/* {this.state.redirect === true && (
+          <Redirect
+            from={{ pathname: "Login" }}
+            to={{ pathname: `/SignUpPage` }} */}
+        /> )}
+        {
+          <div>
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Group controlId="email" bssize="large">
+                <Form.Control
+                  autoFocus
+                  type="email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="password" bssize="large">
+                <Form.Control
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                  type="password"
+                />
+              </Form.Group>
+              <Button
+                // block
+                bssize="large"
+                disabled={!this.validateForm()}
+                type="submit"
+              >
+                Login
+              </Button>
+            </Form>
+            <Link to={`/SignUpPage`}>
+              <Button type="signup">Sign Up</Button>
+            </Link>
+          </div>
+        }
       </div>
     );
   }
 }
+
+function mapStateToProps(state, props) {
+  // props.post = getPost(state, props.params.objectId);
+  // console.log(props.post);
+
+  return {};
+}
+
+Login.propTypes = {
+  user: PropTypes.arrayOf(
+    PropTypes.shape({
+      username: PropTypes.string
+    })
+  ),
+  dispatch: PropTypes.func.isRequired
+};
+
+export default withRouter(connect(mapStateToProps)(Login));
--- client/modules/Post/PostActions.js
+++ client/modules/Post/PostActions.js
@@ -9,7 +9,8 @@ export const ADD_REPLY = "ADD_REPLY";
 export const ADD_REPLIES = "ADD_REPLIES";
 
 // Export Actions
-export function addPost(post) { // this is a Redux action creator
+export function addPost(post) {
+  // this is a Redux action creator
   return {
     type: ADD_POST,
     post
@@ -29,8 +30,8 @@ export function addPostRequest(post) {
         replyDataStructure: post.replys
       }
     }).then(res => dispatch(addPost(res.post))); // dispatch updates the store by adding the action
-                                                // actions describe what happens but don't describe how the app changes
-                                                // reducers specify how the app's state changes in response to the actions sent
+    // actions describe what happens but don't describe how the app changes
+    // reducers specify how the app's state changes in response to the actions sent
   };
 }
 
@@ -139,17 +140,18 @@ export function addReplyRequest(reply) {
 export function fetchReplies(postId) {
   return dispatch => {
     return callApi(`queryReplies/${postId}`).then(res =>
-      dispatch(addReplies(res.replies.results))
+      dispatch(addReplies(res.post.results))
     );
   };
 }
 
 export function fetchLogin(username, password) {
+  console.log("fetch posts");
+
   return dispatch => {
     return callApi(`userLogin/${username}/${password}`).then(res =>
-      dispatch(addUser(res.user)).then(res => console.log(res))
+      dispatch(addUser(res.post))
     );
-    
   };
 }
 
